0f1c8cc7a1480d9c0c3d7ba085696d00bc640d79
Calvyn82 <claytonflesher@gmail.com>
2016-02-17 17:20:03 -0600

    Proof of concept!

--- hamming/hamming.elm
+++ hamming/hamming.elm
@@ -4,7 +4,6 @@ import Html.Attributes exposing (..)
 import Signal exposing (Address)
 import StartApp.Simple as StartApp
 import String
-import List
 
 main =
   StartApp.start { model = emptyModel, view = view, update = update }
@@ -31,7 +30,9 @@ update action model =
 view : Address Action -> Model -> Html
 view address model =
   let validationMessage =
-    if String.length model.string1 == String.length model.string2 then
+    if String.length model.string1 == 0 || String.length model.string2 == 0 then
+       span [style [("color", "purple")]] [text "Please enter two strings"]
+    else if String.length model.string1 == String.length model.string2 then
        span [style [("color", "green")]] [text "Strings are valid!"]
     else
        span [style [("color", "red")]] [text "Strings must be the same length"]
@@ -41,6 +42,7 @@ view address model =
        [ field "text" address String1 "First String" model.string1
        , field "text" address String2 "Second String" model.string2
        , div [] [ text (toString (setHamming model.string1 model.string2))]
+       , div [myStyle] [validationMessage]
        ]
 
 field : String -> Address Action -> (String -> Action) -> String -> String -> Html
@@ -68,16 +70,7 @@ myStyle =
 setHamming : String -> String -> Int
 setHamming string1 string2 =
   if String.length string1 == String.length string2 then
-     List.map2(,) (String.toList string1) (String.toList string2)
-     |> List.filterMap isDiff
-     |> List.length
+     List.map2 (\a b -> if a == b then 0 else 1) (String.toList string1) (String.toList string2)
+     |> List.sum
   else
      0
-    
-
-isDiff : List (List String) -> List Bool
-isDiff list =
-  if List.head list == List.tail list then
-     False
-  else
-     True
